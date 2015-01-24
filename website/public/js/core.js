cf61e78c319e807182b88739a240817e738899d3
papa99do <papa99do@gmail.com>
2015-01-24 11:25:14 +1100

    support read-only mode and edit mode

--- website/public/js/core.js
+++ website/public/js/core.js
@@ -1,4 +1,7 @@
 renebuyApp
+.controller('MainCtrl', function($scope) {
+	$scope.editMode = false;
+})
 .filter('percentage', ['$filter', function ($filter) {
 	return function (input, decimals) {
 	    return $filter('number')(input * 100, decimals) + '%';
@@ -8,4 +11,4 @@ renebuyApp
   	editableThemes.bs3.inputClass = 'input-sm';
   	editableThemes.bs3.buttonsClass = 'btn-sm';
   	editableOptions.theme = 'bs3';
-});;
+});
