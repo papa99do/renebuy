8f8d1f523d76d129ff7998c5b704af57e771ff36
Yihan Zhao <yihanzhao@gmail.com>
2016-04-12 21:59:01 +1000

    get price from chemist warehouse

--- website/app/handlers/dw-price-updater.js
+++ website/app/handlers/dw-price-updater.js
@@ -1,11 +1,13 @@
 var Promise = require('promise');
 var _ = require('lodash');
+var request = require('request');
+var cheerio = require('cheerio');
+
 var Product  = require('../models/product');
-var PriceCollector = require('./price-collector');
 
 
 function updatePrices() {
-  findWatchedProducts().then(getCWStoreInfo).then(printOut);
+  findWatchedProducts().then(getCWStoreInfo).then(printOut).then(getPrices);
 }
 
 function findWatchedProducts() {
@@ -21,17 +23,44 @@ function getCWStoreInfo(products) {
   return products.map(function(p) {
     var cwStore = _.find(p.stores, {storeName: 'CW'});
     return {
+      id: p.id,
       name: p.name,
       rrp: p.rrp,
       price: cwStore.price,
       lowestPrice: cwStore.lowestPrice,
-      productId: cwStore.productId
+      productId: cwStore.productId,
+      detailUrl: cwStore.detailUrl
     };
   });
 }
 
 function printOut(watchedProducts) {
   console.log(watchedProducts);
+  return watchedProducts;
+}
+
+function getPrices(products) {
+  products.forEach(function(p) {
+    getDetailPage(p).then(parsePrice).then(function(newPrice) {printNewPrice(newPrice, p)});
+  });
+}
+
+function getDetailPage(p) {
+  return new Promise(function(resolve, reject) {
+    request(p.detailUrl, function(err, response, html) {
+      if (err) return reject(err);
+      resolve(html);
+    })
+  });
+}
+
+function parsePrice(html) {
+  var price = cheerio.load(html)('[itemprop=price]').text();
+  return price;
+}
+
+function printNewPrice(price, p) {
+  console.log("Price for '%s' is %s", p.name, price);
 }
 
 module.exports = {
--- website/app/jobs.js
+++ website/app/jobs.js
@@ -2,7 +2,7 @@ var schedule = require('node-schedule');
 var dwPriceUpdater = require('./handlers/dw-price-updater');
 
 // run sheduled job to update price every day
-var updatePriceJobCron = '10 * * * *'; 
+var updatePriceJobCron = '*/20 20 * * * *'; 
 function scheduleUpdateDwPriceJob() {
   schedule.scheduleJob(updatePriceJobCron, function() {
   	dwPriceUpdater.updatePrices();
