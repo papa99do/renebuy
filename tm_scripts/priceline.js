8d69f12aea21feeb69ae5c958e0599afec9227ab
Yihan Zhao <yihanzhao@gmail.com>
2016-04-23 10:20:42 +1000

    update priceline tm script

--- tm_scripts/priceline.js
+++ tm_scripts/priceline.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name       Priceline enhancements
 // @namespace  http://yihanzhao.com/
-// @version    0.1
+// @version    0.2
 // @description Priceline enhancements
 // @match      https://www.priceline.com.au/*
 // @copyright  2015+, Yihan Zhao, yihanzhao@gmail.com
 // @require http://code.jquery.com/jquery-latest.js
 // @require http://renebuy.yihanzhao.com/js/monkey/renebuy-enhance.js?1
+// @updateURL https://raw.githubusercontent.com/papa99do/renebuy/master/tm_scripts/priceline.js
+// @downloadURL https://raw.githubusercontent.com/papa99do/renebuy/master/tm_scripts/priceline.js
 // @grant      GM_xmlhttpRequest
 // @grant       GM_addStyle
 // ==/UserScript==
@@ -17,37 +19,36 @@ var reneBuyUrl = "http://renebuy.yihanzhao.com/api/product";
 var PL_URL = "https://www.priceline.com.au";
 
 $(document).ready(function() {
-	
+
 	function addEnhanceBtn(enhanceBtnHtml) {
-		$('.product-box').each(function() {
+		$('.item').each(function() {
 	        $(this).append(enhanceBtnHtml);
 	    });
     }
-    
+
     function extractProductInfo($enhanceBtn, extractNumber) {
     	// extract product information
         var productElem = $enhanceBtn.parent().parent();
-        var name = productElem.find('.product-img').attr('title').substring('Click for more info: '.length);
-        var detailUrl = productElem.find('.product-img > a').attr('href');
-        var photoUrl = productElem.find('.product-img img').attr('src');
-        var id = extractNumber(/(\d+)_thumb\.jpg/, photoUrl);
-        var price = extractNumber(/\$([0-9.]+)/, productElem.find('.product-price2').text());
-        var rrpText = productElem.find('.product-price1').text();
-        var rrp = rrpText ? extractNumber(/Was \$([0-9.]+)/, rrpText) : price;
-        
+        var name = productElem.find('.product-name > a').attr('title');
+        var detailUrl = productElem.find('.product-name > a').attr('href');
+        var photoUrl = productElem.find('.product-image-container img').attr('src');
+        var id = extractNumber(/(\d+)/, productElem.find('button').attr('data-product-id'));
+        var price = extractNumber(/\$([0-9.]+)/, productElem.find('.price-box .price').text());
+        var rrp = price;
+
         var product = {
             productId: id,
             name: name.trim(),
             price: price,
             rrp: rrp,
             photos: [photoUrl],
-            detailUrl: PL_URL + detailUrl,
+            detailUrl: detailUrl,
 			store: 'PL'
         };
-       
+
         return product;
     }
-    
+
     renebuy($, GM_xmlhttpRequest, GM_addStyle, addEnhanceBtn, extractProductInfo, reneBuyUrl).init();
-    
+
 });
